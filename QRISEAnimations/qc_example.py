c4aeffc78c5dad94b171257d550e27026e1d76d6
Kevin Li <klyiming@gmail.com>
2024-04-12 21:21:02 -0400

    new example

--- QRISEAnimations/qc_example.py
+++ QRISEAnimations/qc_example.py
@@ -12,19 +12,16 @@ from qiskit import QuantumCircuit
 from qiskit.providers.fake_provider import GenericBackendV2
  
 backend = GenericBackendV2(5)
+
+theta = 2 * np.arccos(1/np.sqrt(3))
  
-qc = QuantumCircuit(4, 1)
+qc = QuantumCircuit(3, 1)
  
-qc.h(0)
-qc.h(3)
+qc.ry(theta, 0)
+qc.ch(0, 1)
+qc.cx(1, 2)
+qc.cx(0, 1)
 qc.x(0)
-qc.x(3)
-qc.ccx(0, 3, 1)
-qc.x(0)
-qc.x(3)
-qc.ccx(0, 3, 2)
-qc.cx(2, 0)
-qc.cx(2, 3)
 
 qc.measure([0], [0])
 
